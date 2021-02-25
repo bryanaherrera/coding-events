82b4e9ee97f3c80417f638d8f99cb2a00ece0bf5
bryanaherrera <bryanaherrera98@gmail.com>
2021-02-24 18:47:57 -0600

    Initial commit

---
+++ src/main/java/org/launchcode/demo/controllers/EventController.java
@@ -0,0 +1,38 @@
+package org.launchcode.demo.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("events")
+public class EventController {
+
+    private static List<String> events = new ArrayList<>();
+
+    @GetMapping
+    public String displayAllEvents(Model model){
+        model.addAttribute("events", events);
+        return "events/index";
+    }
+
+    // lives at /events/create
+    @GetMapping("create")
+    public String renderCreateEventForm(){
+        return "events/create";
+    }
+
+    // lives at /events/create
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName){
+        events.add(eventName);
+        return "redirect:";
+    }
+}
